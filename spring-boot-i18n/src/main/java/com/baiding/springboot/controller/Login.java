a605801b528dbb6325a4fb67e6687f21761c257f
jun.liu <jun.liu@wifiin.com>
2018-09-19 17:25:02 +0800

    Spring Boot 2.0.5 国际化

---
+++ spring-boot-i18n/src/main/java/com/baiding/springboot/controller/Login.java
@@ -0,0 +1,17 @@
+package com.baiding.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Author: BaiDing
+ * @Date: 2018/9/19 11:51
+ * @Email: liujiabaiding@foxmail.com
+ */
+@Controller
+public class Login {
+
+//    @RequestMapping("/login")
+//    public String login(){
+//        return "login";
+//    }
+}
